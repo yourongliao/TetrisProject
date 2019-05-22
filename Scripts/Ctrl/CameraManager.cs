6ab87d8ff5a36456f146eb0fbf1eabfdf2dd0a05
yourongliao <yourongliao@protonmail.com>
2019-05-22 22:49:07 +0800

    code

---
+++ Scripts/Ctrl/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraManager : MonoBehaviour {
+
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+
+    public void ZoomIn()
+    {
+        mainCamera.DOOrthoSize(14f, 0.5f);
+    }
+
+    public void ZoomOut()
+    {
+        mainCamera.DOOrthoSize(20.35f, 0.5f);
+    }
+}
